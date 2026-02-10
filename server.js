b5a9b0c332dbbf2a02629580915b8a58c6c39e5e
makrem <snoussimakrem6@gmail.com>
2026-02-10 14:55:37 +0100

    back url

--- server.js
+++ server.js
@@ -42,12 +42,28 @@ app.use(helmet({
 }));
 
 // CORS configuration - more permissive for development
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
+  'https://competitive-programming-club-front.vercel.app'
+];
+
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: function (origin, callback) {
+    // Allow requests with no origin (Postman, server-to-server)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Origin', 'Accept']
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 // Log all requests
 app.use((req, res, next) => {
