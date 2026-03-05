38ca81cf29a33f9d0ed83bbef2ac9926cc0b7011
snoussi makrem <snoussimakrem6@gmail.com>
2026-03-05 15:04:28 +0100

    Add new allowed origin for CORS

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
   'https://competitive-programming-club-front.vercel.app'
+  'https://cpcenicarthage.tn'
 ];
 
 app.use(cors({
